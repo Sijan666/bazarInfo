5624a55188b7679201ba6933d21ba0a3ad0cacb3
Sijan666 <666majharulislam@gmail.com>
2026-03-07 14:37:34 +0600

    changes in home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,16 @@
-
-import Banner from "../layouts/Banner"
-
+import React from 'react'
+import MarketStats from '../layouts/MarketStats'
+import FilterControls from '../layouts/FilterControls'
+import ProductCard from '../layouts/ProductCard'
 
 const Home = () => {
-    return (
-        <>
-        <Banner/>
-        </>
-    )
+  return (
+    <>
+    <MarketStats/>
+    <FilterControls/>
+    <ProductCard/>
+    </>
+  )
 }
 
 export default Home
