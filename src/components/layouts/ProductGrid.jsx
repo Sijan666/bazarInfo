30dd9aa0e8c1c130b1d8c83033752dbd20857fa3
Majharul Islam <666majharulislam@gmail.com>
2026-04-30 23:24:04 +0600

    removed unwanted line or codes

--- src/components/layouts/ProductGrid.jsx
+++ src/components/layouts/ProductGrid.jsx
@@ -17,7 +17,6 @@ const ProductGrid = () => {
         </div>
         );
     }
-
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {filteredProducts.map((product) => (
