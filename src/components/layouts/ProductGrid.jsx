447168cf1e2f26a182c4e2fe7611b1007dab7469
Sijan666 <666majharulislam@gmail.com>
2026-06-03 23:46:19 +0600

    update product grid

--- src/components/layouts/ProductGrid.jsx
+++ src/components/layouts/ProductGrid.jsx
@@ -18,7 +18,7 @@ const ProductGrid = () => {
         );
     }
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-5">
         {filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
         ))}
